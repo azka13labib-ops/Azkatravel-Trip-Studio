6b5feccfc1e588c5c88da6430d914ac3f5e49f0c
Azka Labib <azka13labib@gmail.com>
2026-01-11 22:49:20 +0700

    fix: vite alias + base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 export default defineConfig({
+  base: '/website-travel/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
